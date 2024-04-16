cbd87c2068ef3bba839e352ea54911adfc5db719
belcaid <belcaid.abdellah2001@gmail.com>
2024-04-16 15:50:41 +0100

    feat: Set up front-end project structure and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-
-
 function App() {
- 
-
   return (
     <>
       <div>
-  <h1>Tailwind header</h1>
+        <h1>Tailwind header</h1>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
